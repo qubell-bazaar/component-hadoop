f9a8e2c2cae873fc32857300586b4992879e8465
Anton Butovich <abutovich@gmail.com>
2014-10-24 18:49:10 +0300

    Fix test to check responce code on CM authorization

--- test/test.py
+++ test/test.py
@@ -28,12 +28,12 @@ class ClouderaHadoopComponentTestCase(BaseComponentTestCase):
     def timeout(cls):
         return 60
     @instance(byApplication=name)
-    @values({"Management.Manager": "hosts"})
-    def test_port(self, instance, hosts, port=7180):
-        import socket
-        import time
-        time.sleep(60)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((hosts, int(port)))
+    @values({"Cloudera.Manager_URL": "url"})
+    def test_check_user_login(self, instance, url):
+      import socket
+      import requests 
 
-        assert result == 0
+      timeout = 10
+      socket.setdefaulttimeout(timeout)
+      response = requests.get(url, auth=requests.auth.HTTPBasicAuth('admin', 'admin'))
+      self.assertEqual(200, response.status_code)
