89e737e67feac0d21e5bfc3c17059ca77abce4d4
Anton Butovich <abutovich@gmail.com>
2014-12-24 18:24:40 +0200

    Use cookbook hadoop v 2.1.0

--- test/test.py
+++ test/test.py
@@ -18,21 +18,61 @@ from qubell.api.testing import *
 })
 class ClouderaHadoopComponentTestCase(BaseComponentTestCase):
     name = "CDH Main"
-    meta = "https://raw.githubusercontent.com/qubell-bazaar/component-hadoop/components/meta.yml"
-    apps = [{
-        "name": name,
-        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../component-hadoop.yml'))
-    }]
+    #meta = os.path.realpath(os.path.join(os.path.dirname(__file__), '../meta.yml'))
+    apps = [
+       {"name": name,
+        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../component-hadoop.yml')),
+        "settings": {"destroyInterval": 14400000}},
+       {"name": "Cloudera Flume",
+        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-flume.yml')),
+        "launch": False},
+       {"name": "Cloudera Hadoop",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-hadoop.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Hive",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-hive.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Hue",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-hue.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Impala",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-impala.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Manager",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-manager.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Oozie",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-oozie.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Pig",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-pig.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Solr",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-solr.yml')),
+       "launch": False
+       },
+       {"name": "Cloudera Sqoop",
+       "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../cloudera-sqoop.yml')),
+       "launch": False
+       }
+    ]
     @classmethod
     def timeout(cls):
-        return 90
+        return 90 
     @instance(byApplication=name)
-    @values({"Cloudera.manager": "url"})
-    def test_check_user_login(self, instance, url):
+    def test_check_user_login(self, instance):
       import socket
       import requests 
 
       timeout = 10
       socket.setdefaulttimeout(timeout)
+      url = instance.returnValues['Cloudera.manager']
       response = requests.get(url, auth=requests.auth.HTTPBasicAuth('admin', 'admin'))
       self.assertEqual(200, response.status_code)
